8e5d0e4fc8a5efe9b6a09b03cd89f53410a9958b
Eklent <ismailieklent3@gmail.com>
2022-02-08 16:47:57 +0100

    Created Table component

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useMemo } from 'react';
-import { useTable } from 'react-table';
 import { usersColumns } from '../tableColumns/usersColumns';
 import axios from 'axios';
+import Table from '../components/table/Table';
 
 function Users() {
   const [usersData, setUsersData] = useState([]);
@@ -18,68 +18,10 @@ function Users() {
     setLoading(false);
   }
 
-  const tableInstance = useTable({ columns: usersColumns, data: usersData });
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    tableInstance;
-
   return (
     <div className="page">
       Users
-      <table {...getTableProps()}>
-        <thead>
-          {
-            // Loop over the header rows
-            headerGroups.map(headerGroup => (
-              // Apply the header row props
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {
-                  // Loop over the headers in each row
-                  headerGroup.headers.map(column => (
-                    // Apply the header cell props
-                    <th {...column.getHeaderProps()}>
-                      {
-                        // Render the header
-                        column.render('Header')
-                      }
-                    </th>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </thead>
-
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {
-            // Loop over the table rows
-            rows.map(row => {
-              // Prepare the row for display
-              prepareRow(row);
-              return (
-                // Apply the row props
-                <tr {...row.getRowProps()}>
-                  {
-                    // Loop over the rows cells
-                    row.cells.map(cell => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
-                          {
-                            // Render the cell contents
-                            cell.render('Cell')
-                          }
-                        </td>
-                      );
-                    })
-                  }
-                </tr>
-              );
-            })
-          }
-        </tbody>
-      </table>
+      <Table columns={usersColumns} data={usersData} />
     </div>
   );
 }
