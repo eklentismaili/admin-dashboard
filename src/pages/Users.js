7339a6afdee7f63468ea2f5acb740dfc33362910
Eklent <ismailieklent3@gmail.com>
2022-02-08 16:41:42 +0100

    Added react-table mockup in users page

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,5 +1,87 @@
+import { useEffect, useState, useMemo } from 'react';
+import { useTable } from 'react-table';
+import { usersColumns } from '../tableColumns/usersColumns';
+import axios from 'axios';
+
 function Users() {
-  return <div>Users</div>;
+  const [usersData, setUsersData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsersData();
+  }, []);
+
+  async function fetchUsersData() {
+    const res = await axios.get(`https://reqres.in/api/users`);
+    setUsersData(res.data.data);
+    console.log(res);
+    setLoading(false);
+  }
+
+  const tableInstance = useTable({ columns: usersColumns, data: usersData });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <div className="page">
+      Users
+      <table {...getTableProps()}>
+        <thead>
+          {
+            // Loop over the header rows
+            headerGroups.map(headerGroup => (
+              // Apply the header row props
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {
+                  // Loop over the headers in each row
+                  headerGroup.headers.map(column => (
+                    // Apply the header cell props
+                    <th {...column.getHeaderProps()}>
+                      {
+                        // Render the header
+                        column.render('Header')
+                      }
+                    </th>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </thead>
+
+        {/* Apply the table body props */}
+        <tbody {...getTableBodyProps()}>
+          {
+            // Loop over the table rows
+            rows.map(row => {
+              // Prepare the row for display
+              prepareRow(row);
+              return (
+                // Apply the row props
+                <tr {...row.getRowProps()}>
+                  {
+                    // Loop over the rows cells
+                    row.cells.map(cell => {
+                      // Apply the cell props
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {
+                            // Render the cell contents
+                            cell.render('Cell')
+                          }
+                        </td>
+                      );
+                    })
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Users;
---
+++ src/tableColumns/usersColumns.js
@@ -0,0 +1,22 @@
+export const usersColumns = [
+  {
+    Header: 'Id',
+    accessor: 'id',
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+  },
+  {
+    Header: 'First Name',
+    accessor: 'first_name',
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name',
+  },
+  {
+    Header: 'Avatar',
+    accessor: 'avatar',
+  },
+];
